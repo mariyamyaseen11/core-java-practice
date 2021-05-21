3d9f4e13dc6d2e5ad79357a83579d1c634e119f3
Taha <mohmmady@dewsolutions.in>
2021-05-21 19:34:59 +0530

    reverse string using builtin function

---
+++ src/com/string/reverse.java
@@ -0,0 +1,19 @@
+package com.string;
+						
+public class reverse {
+	
+	public static void main(String args[]) {
+		
+		String input ="Mariyam Yaseen";
+		
+		
+		StringBuilder input1 = new StringBuilder();
+		
+		 input1.append(input);
+		 
+		 input1.reverse();
+		
+		 System.out.println(input1);
+	}
+
+}
